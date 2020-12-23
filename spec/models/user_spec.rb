82e20cde11bc2d56fbe6cfe71de24eb32587eac0
TOSHI-hiro <inchs528@gmail.com>
2020-12-23 11:55:21 +0900

    修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,12 +26,12 @@ RSpec.describe User, type: :model do
       @user.password_confirmation = "1234567"
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-      it "重複したemailが存在する場合登録できないこと" do
+    end
+   it "重複したemailが存在する場合登録できないこと" do
         @user.save
       another_user = FactoryBot.build(:user, email: @user.email)
       another_user.valid?
       expect(another_user.errors.full_messages).to include("Email has already been taken")
-
+      end
     end
   end
-end
